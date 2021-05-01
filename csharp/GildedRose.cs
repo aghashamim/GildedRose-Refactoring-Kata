75ea60beec927420e63b214a5766b3343963dd71
Agha Shamim <AghaShamim@LAPTOP>
2021-05-01 15:25:29 +0800

    Rearrange code into code blocks for each item type as per the requirements.

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -4,7 +4,7 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        readonly IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -15,101 +15,87 @@ namespace csharp
             // Go through each item
             foreach (var item in Items)
             {
-                // For items of type other than Aged Brie and Backstage passes
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                // When the item is of type Aged Brie
+                if (item.Name == "Aged Brie")
                 {
-                    // When the quality is greater than 0
-                    if (item.Quality > 0)
+                    // and the quality is less than the maximum allowed of 50
+                    if (item.Quality < 50)
                     {
-                        // and item type is other than Sulfuras
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            // Decrease quality by 1
-                            item.Quality = item.Quality - 1;
-                        }
+                        // Increase the quality by 1
+                        item.Quality = item.Quality + 1;
                     }
+
+                    // and the sell in days have passed
+                    if (item.SellIn <= 0)
+                    {
+                        // Decrease quality by 1
+                        item.Quality = item.Quality + 1;
+                    }
+
+                    // Decrease the sell in days by 1
+                    item.SellIn = item.SellIn - 1;
                 }
-                // For items of type Aged Brie and Backstage passes
-                else
+
+                // When the item is of type Sulfuras
+                else if (item.Name == "Sulfuras, Hand of Ragnaros")
                 {
-                    // When the quality is less than the maximum allowed of 50
+                    // Do nothing, since it's a legendary item
+                }
+
+                // When the item is of type Backstage passes
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    // and the quality is less than the maximum allowed of 50
                     if (item.Quality < 50)
                     {
                         // Increase the quality by 1
                         item.Quality = item.Quality + 1;
 
-                        // When the item is of type Backstage passes
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        // and there are 10 or less days remaining to sell
+                        if (item.SellIn < 11)
                         {
-                            // and there are 10 or less days remaining to sell
-                            if (item.SellIn < 11)
-                            {
-                                // and the quality is less than the maximum allowed of 50
-                                if (item.Quality < 50)
-                                {
-                                    // Increase the quality by 1
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            // and there are 5 or less days remaining to sell
-                            if (item.SellIn < 6)
-                            {
-                                // and the quality is less than the maximum allowed of 50
-                                if (item.Quality < 50)
-                                {
-                                    // Increase the quality by 1
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
+                            // Increase the quality by 1
+                            item.Quality = item.Quality + 1;
                         }
-                    }
-                }
 
-                // For items of type other than Sulfuras
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    // Decrease the sell in days by 1
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                // When the sell in days have passed
-                if (item.SellIn < 0)
-                {
-                    // For items of type other than Aged Brie
-                    if (item.Name != "Aged Brie")
-                    {
-                        // and items of type other than Backstage passes
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            // When the quality is greater than 0
-                            if (item.Quality > 0)
-                            {
-                                // and the item type is other than Sulfuras
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    // Decrease quality by 1
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        // For item of type Backstage passes
-                        else
-                        {
-                            // Drop the quality to 0
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    // For item of type Aged Brie
-                    else
-                    {
-                        // When the quality is less than the maximum allowed of 50
-                        if (item.Quality < 50)
+                        // and there are 5 or less days remaining to sell
+                        if (item.SellIn < 6)
                         {
                             // Increase the quality by 1
                             item.Quality = item.Quality + 1;
                         }
                     }
+
+                    // When the sell in days have passed
+                    if (item.SellIn <= 0)
+                    {
+                        // Drop the quality to 0
+                        item.Quality = item.Quality - item.Quality;
+                    }
+
+                    // Decrease the sell in days by 1
+                    item.SellIn = item.SellIn - 1;
+                }
+                
+                // When the item is of any other type
+                else
+                {
+                    // When the quality is greater than 0
+                    if (item.Quality > 0)
+                    {
+                        // Decrease quality by 1
+                        item.Quality = item.Quality - 1;
+                    }
+
+                    // When the sell in days have passed
+                    if (item.SellIn <= 0)
+                    {
+                        // Decrease quality by 1
+                        item.Quality = item.Quality - 1;
+                    }
+
+                    // Decrease the sell in days by 1
+                    item.SellIn = item.SellIn - 1;
                 }
             }
         }
